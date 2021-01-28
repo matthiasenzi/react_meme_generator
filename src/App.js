de57127dacd97b0e532ab55523f3c01bb8d2501e
Matthias Enzi <matthias@upleveled.io>
2021-01-28 15:28:47 +0100

    Add insert text functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function MemeGenerator() {
+  // Meme-Text via controlled Components with State variables
+  const [topText, settopText] = useState('');
+  const [bottomText, setbottomText] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>REACT MEME GENERATOR</h1>
+      <p>This meme generator allows you to create memes and put text on it.</p>
+      <p>Please enter your text below:</p>
+      <label>
+        Top Text:{' '}
+        <input
+          // Controlled Component Top Text: onChange of form element
+          onChange={(event) => {
+            settopText(event.currentTarget.value);
+          }}
+          value={topText}
+        />
+      </label>
+      <br />
+      <label>
+        Bottom Text:{' '}
+        <input
+          // Controlled Component Bottom Text: onChange of form element
+          onChange={(event) => {
+            setbottomText(event.currentTarget.value);
+          }}
+          value={bottomText}
+        />
+      </label>
+      <h2>Text Preview</h2>
+      <b>Top Text:</b> {topText}
+      <br />
+      <b>Bottom Text:</b> {bottomText}
+      <br />
     </div>
   );
 }
-
-export default App;
